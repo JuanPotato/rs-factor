40414f706671c5cef0aee73363d920b6ebf42c3e
Hasan Ibraheem <hasantiny@gmail.com>
2017-04-30 00:04:37 -0400

    Added sieve of eratosthenes Might make a new crate for sieves

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,32 @@ pub fn trial_factor(n: i64) -> (i64, i64) {
     return (1, n)
 }
 
+fn sieve_of_eratosthenes(n: u64) /* -> Vec<u64> */ {
+    let mut numbers = (0..n+1).collect::<Vec<u64>>();
+    numbers[1] = 0;
+
+    for i in 2..(n as f64).sqrt().floor() as u64 + 1 {
+        if numbers[i as usize] < 2 {
+            continue;
+        }
+
+        for k in 2 .. n/i + 1 {
+            numbers[(k*i) as usize] = 0;
+        }
+
+        println!("{:?}", i);
+    }
+
+    println!("{:?}", numbers);
+}
+
 
 #[cfg(test)]
 mod tests {
-    use trial_factor;
+    use *;
     #[test]
     fn it_works() {
+        sieve_of_eratosthenes(121);
         println!("{:?}", trial_factor(-4));
     }
 }
